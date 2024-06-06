b328bcc541200fd10e45a170a257279b1fd98e58
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2024-06-06 16:05:17 +0545

    done

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ navLinks.addEventListener("click",(e) => {
     navLinks.classList.toggle("open");
     menuBtnIcon.setAttribute("class","ri-menu-line");
 });
+
+const scrollRevealOption = {
+    distance : "50px",
+    origin:"button",
+    duration:1000,
+
+};
+//header container 
+scrollRevealOption().reveal(".header_contaianer .section_subheader",{
+    ...scrollRevealOption})
+scrollRevealOption().reveal(".header_contaianer h1",{
+        ...scrollRevealOption,
+    delay:500,
+})
+scrollRevealOption().reveal(".header_contaianer .btn",{
+            ...scrollRevealOption, delay:1000,})
